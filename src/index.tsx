0950084f78c1c05d9c546e4e1848d267eb00c6d5
jordles113 <jordan.t.romero@gmail.com>
2022-07-23 17:54:44 -0500

    wires up esbuild

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,34 @@
 import ReactDOM from "react-dom";
-import { useState } from "react";
+import * as esbuild from 'esbuild-wasm'
+import { useState, useEffect, useRef } from "react";
 
 const App = () => {
+    const ref = useRef<any>()
     const [input, setInput] = useState('')
     const [code, setCode] = useState('')
 
-    const onClick = () => {
-        console.log(input)
+    const startService = async () => {
+        ref.current = await esbuild.startService({
+            worker: true, 
+            wasmURL: '/esbuild.wasm'
+        })
+    }
+
+    useEffect(() => {
+      startService()
+    }, [])
+    
+
+    const onClick = async () => {
+        if (!ref.current) {
+            return
+        }
+
+        const result = await ref.current.transform(input, {
+            loader: 'jsx',
+            target: 'es2015'
+        })
+        setCode(result.code)
     }
 
     return <div>
@@ -14,7 +36,7 @@ const App = () => {
         <div>
             <button onClick={onClick}>Submit</button>
         </div>
-        <pre></pre>
+        <pre>{code}</pre>
     </div>
 }
 
