cdb8122de2b099e16becd2570b69b786bd53ead0
jordles113 <jordan.t.romero@gmail.com>
2022-07-25 09:53:02 -0500

    wires up the iframe for displaying code

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>()
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
@@ -35,17 +36,22 @@ const App = () => {
       },
     });
 
-    // console.log(result);
-
-    setCode(result.outputFiles[0].text);
-
-    try {
-      eval(result.outputFiles[0].text)
-    } catch (err) {
-      alert(err)
-    }
-    
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
   };
+
+  const html = `
+    <html>
+    <head></head>
+    <body>
+      <div id="root"></div>
+      <script>
+        window.addEventListener('message', (event) => {
+          eval(event.data)
+        }, false)
+      </script>
+    </body>
+    </html>
+  `;
 
   return (
     <div>
@@ -57,7 +63,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
-      <iframe src="/test.html"></iframe>
+      <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} />
     </div>
   );
 };
